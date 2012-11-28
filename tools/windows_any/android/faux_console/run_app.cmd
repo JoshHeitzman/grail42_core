9782bafd902b2450e0da23570ef5fdffd2175843
Josh Heitzman <joshheitzman@hotmail.com>
2012-11-27 23:20:22 -0800

    Enhancements to Android faux console run app.
    Rename android_home env var to android_sdk_root.
    
    Pass through exit code from remote faux console app.

--- tools/windows_any/android/faux_console/run_app.cmd
+++ tools/windows_any/android/faux_console/run_app.cmd
@@ -11,7 +11,7 @@ setlocal ENABLEDELAYEDEXPANSION
 
 if "%1"=="" goto Usage
 if "%2"=="" goto Usage
-if "%android_home%"=="" goto Usage
+if "%android_sdk_root%"=="" goto Usage
 if "%grail42_core_cmd%"=="" goto Usage
 call "%grail42_core_cmd%\ensure_argument_not_usage_request.cmd" %1
 if ERRORLEVEL 1 goto Usage
@@ -35,7 +35,7 @@ echo.
 echo Prior to execution:
 echo   * The adb server should be running and connected to a single emulator or
 echo     device prior to execution.
-echo   * The android_home environment variable must be set the root to the
+echo   * The android_sdk_root environment variable must be set the root to the
 echo     Android SDK.
 echo   * The grail42_core_cmd environment variable echo is required to be set
 echo     by running set_env_core.cmd.
@@ -43,25 +43,24 @@ echo.
 echo If the grail42_cmd_verbose_logging environment variable is set then 
 echo additional diagnostic output will be emitted.
 echo.
-echo.
 echo For example:
 echo.
-echo set android_home=C:\adt-bundle-windows-x86\sdk
+echo set android_sdk_root=C:\adt-bundle-windows-x86\sdk
 echo RUN_APP com.example.plasma .Plasma
 endlocal & CMD /C EXIT 1
 goto :EOF
 
 :ArgsValidated
 rem Clear the current log state
-"%android_home%\platform-tools\adb.exe" logcat -c
+"%android_sdk_root%\platform-tools\adb.exe" logcat -c
 rem Start the activity in the package
-"%android_home%\platform-tools\adb.exe" shell am start -n %1/%2
+"%android_sdk_root%\platform-tools\adb.exe" shell am start -n %1/%2
 if ERRORLEVEL 1 endlocal & CMD /C EXIT 1 & goto :EOF
 
 :WaitForLogToHaveEntries
 if defined grail42_cmd_verbose_logging echo WaitForLogToHaveEntries
-"%android_home%\platform-tools\adb.exe" logcat -g | findstr /C:"0Kb consumed" > nul
-if not ERRORLEVEL 1 goto LogHasEntries
+"%android_sdk_root%\platform-tools\adb.exe" logcat -g | findstr /C:"0Kb consumed" > nul
+if ERRORLEVEL 1 goto LogHasEntries
 call "%grail42_core_cmd%\sleep.cmd" 1
 goto WaitForLogToHaveEntries
 :LogHasEntries
@@ -73,13 +72,13 @@ set iterations=1
 if defined grail42_cmd_verbose_logging echo WaitForRemoteAppExecutionToStart
 rem These identifiers are emitted by the FauxConsoleActivity included in the template and are used to determine that app has actually started.
 rem It is possible for the app to start and not emit these identifier due to a preceding error.
-"%android_home%\platform-tools\adb.exe" logcat -d -v raw 13380324612F4E3184624E7AD706141A:I *:S | findstr "4D85BA47C2364027A09FBF016A5C2C2A" > nul
+"%android_sdk_root%\platform-tools\adb.exe" logcat -d -v raw 13380324612F4E3184624E7AD706141A:I *:S | findstr "4D85BA47C2364027A09FBF016A5C2C2A" > nul
 if not ERRORLEVEL 1 goto RemoteAppExecutionStarted
 if /i %iterations% GEQ 4 (
 	echo.
     echo ERROR - app execution start was not detected after %iterations% attempts.  Dumping log data.
 	echo.
-	if defined grail42_cmd_verbose_logging ("%android_home%\platform-tools\adb.exe" logcat -d) else ("%android_home%\platform-tools\adb.exe" logcat -d -v raw stderr:I stdout:I *:S)
+	if defined grail42_cmd_verbose_logging ("%android_sdk_root%\platform-tools\adb.exe" logcat -d) else ("%android_sdk_root%\platform-tools\adb.exe" logcat -d -v raw stderr:I stdout:I *:S)
 	endlocal & CMD /C EXIT 1 & goto :EOF
 )
 call "%grail42_core_cmd%\sleep.cmd" 1
@@ -93,7 +92,7 @@ if defined grail42_cmd_verbose_logging echo RemoteAppExecutionStarted
 if defined grail42_cmd_verbose_logging echo WaitForRemoteAppExecutionToComplete
 rem These identifiers are emitted by the FauxConsoleActivity included in the template and are used to determine that app has finished execution.
 rem It is possible for the app to terminate and not emit these identifier due to a preceding error.
-"%android_home%\platform-tools\adb.exe" logcat -d -v raw 13380324612F4E3184624E7AD706141A:I *:S | findstr "ECD3B7A023654F9D99F2EF3D4AA33925" > nul
+"%android_sdk_root%\platform-tools\adb.exe" logcat -d -v raw 13380324612F4E3184624E7AD706141A:I *:S | findstr "DAB25FDC5A974392AA39ED7928A31561" > nul
 if not ERRORLEVEL 1 goto RemoteAppExecutionComplete
 call "%grail42_core_cmd%\sleep.cmd" 1
 goto WaitForRemoteAppExecutionToComplete
@@ -102,4 +101,6 @@ if defined grail42_cmd_verbose_logging echo RemoteAppExecutionComplete
 
 rem Scan through the log lines and only emit the lines between the start and end markers.
 set enable_stdout_echo=
-for /f "delims=" %%i in ('"%android_home%\platform-tools\adb.exe" logcat -d -v raw 13380324612F4E3184624E7AD706141A:I stderr:I stdout:I *:S') do (call "%grail42_core_cmd%\if_equ_set.cmd" %%i ECD3B7A023654F9D99F2EF3D4AA33925 enable_stdout_echo) && (if "!enable_stdout_echo!"=="1" echo %%i) && (call "%grail42_core_cmd%\if_equ_set.cmd" %%i 4D85BA47C2364027A09FBF016A5C2C2A enable_stdout_echo 1)
+set remote_exit_code=0
+for /f "delims=" %%i in ('"%android_sdk_root%\platform-tools\adb.exe" logcat -d -v raw 13380324612F4E3184624E7AD706141A:I stderr:I stdout:I *:S') do (call "%grail42_core_cmd%\if_equ_set.cmd" %%i ECD3B7A023654F9D99F2EF3D4AA33925 enable_stdout_echo) && (if "!enable_stdout_echo!"=="1" echo %%i) && (call "%grail42_core_cmd%\if_equ_set.cmd" %%i 4D85BA47C2364027A09FBF016A5C2C2A enable_stdout_echo 1) && (for /f "tokens=1,2 delims=:" %%r in ("%%i") do call "%grail42_core_cmd%\if_equ_set.cmd" %%r DAB25FDC5A974392AA39ED7928A31561 remote_exit_code %%s)
+endlocal & CMD /C EXIT %remote_exit_code%
