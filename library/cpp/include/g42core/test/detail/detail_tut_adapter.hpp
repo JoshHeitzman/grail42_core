d73cd6f3afe010c57964f7a8242b3a2a2480c2db
Josh Heitzman <joshheitzman@hotmail.com>
2012-12-13 21:50:35 -0800

    Empty headers for tut adapter.
    Both the internal detail header and the public header are included.

---
+++ library/cpp/include/g42core/test/detail/detail_tut_adapter.hpp
@@ -0,0 +1,16 @@
+/*                   Copyright (C) 2012 Josh Heitzman
+Distributed under the Boost Software License, Version 1.0.
+See accompanying file LICENSE_1_0.txt or online copies at:
+* http://www.boost.org/LICENSE_1_0.txt
+* http://opensource.org/licenses/BSL-1.0
+* http://directory.fsf.org/wiki/License:Boost1.0
+* http://en.wikipedia.org/wiki/Boost_Software_License                */
+
+#ifndef MACRO_PREFIX_HG_C03ABA22C2A04F5FB419E6A771232810
+#define MACRO_PREFIX_HG_C03ABA22C2A04F5FB419E6A771232810
+#pragma warning(push)
+#pragma warning(disable : 4068)
+#pragma once
+#pragma warning(pop)
+
+#endif // MACRO_PREFIX_HG_C03ABA22C2A04F5FB419E6A771232810
---
+++ library/cpp/include/g42core/test/tut_adapter.hpp
@@ -0,0 +1,16 @@
+/*                   Copyright (C) 2012 Josh Heitzman
+Distributed under the Boost Software License, Version 1.0.
+See accompanying file LICENSE_1_0.txt or online copies at:
+* http://www.boost.org/LICENSE_1_0.txt
+* http://opensource.org/licenses/BSL-1.0
+* http://directory.fsf.org/wiki/License:Boost1.0
+* http://en.wikipedia.org/wiki/Boost_Software_License                */
+
+#ifndef MACRO_PREFIX_HG_B51A3C840B644C4E8D02FB145152DC3D
+#define MACRO_PREFIX_HG_B51A3C840B644C4E8D02FB145152DC3D
+#pragma warning(push)
+#pragma warning(disable : 4068)
+#pragma once
+#pragma warning(pop)
+
+#endif // MACRO_PREFIX_HG_B51A3C840B644C4E8D02FB145152DC3D
