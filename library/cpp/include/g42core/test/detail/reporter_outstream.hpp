faca0500ae2e50a4189e950c1e80c46f51f3d507
Josh Heitzman <joshheitzman@hotmail.com>
2012-12-14 22:46:26 -0800

    Add reporter_outstream and its unit tests.

---
+++ library/cpp/include/g42core/test/detail/reporter_outstream.hpp
@@ -0,0 +1,163 @@
+/*                   Copyright (C) 2012 Josh Heitzman
+Distributed under the Boost Software License, Version 1.0.
+See accompanying file LICENSE_1_0.txt or online copies at:
+* http://www.boost.org/LICENSE_1_0.txt
+* http://opensource.org/licenses/BSL-1.0
+* http://directory.fsf.org/wiki/License:Boost1.0
+* http://en.wikipedia.org/wiki/Boost_Software_License                */
+
+#ifndef G42CORE_HG_A533B033358A43CB9FC53C9C607EC0AE
+#define G42CORE_HG_A533B033358A43CB9FC53C9C607EC0AE
+#pragma warning(push)
+#pragma warning(disable : 4068)
+#pragma once
+#pragma warning(pop)
+
+G42CORE_TEST_BEGIN_NAMESPACES
+
+namespace detail
+{
+
+// TODO BEGIN find outstream_append_* struct and typdefs a new home
+// probably in an outstream namespace, so outstream need not be in the typenames.
+
+struct outstream_append_nothing
+{
+    template <typename OutStream>
+    inline static void append_to_stream(OutStream&)
+    {
+    }
+};
+
+template <typename T, T Value>
+struct outstream_append_value
+{
+    template <typename OutStream>
+    inline static void append_to_stream(OutStream& outStream)
+    {
+        outStream << Value;
+    }
+};
+
+// Prefered over std::endl as it doesn't have any side effects (e.g. flushing the stream).
+typedef outstream_append_value<char, '\n'> outstream_append_slashn;
+
+// END TODO (see above)
+
+template <typename LineEnding = outstream_append_slashn>
+struct reporter_outstream_formatter_line_common_char
+{
+    struct prefix
+    {
+        template <typename OutStream>
+        inline static void append_to_stream(OutStream& outStream)
+        {
+            outStream << "*** Tests ";
+        }
+    };
+    struct postfix
+    {
+        template <typename OutStream>
+        inline static void append_to_stream(OutStream& outStream)
+        {
+            outStream << " ***";
+            LineEnding::append_to_stream(outStream);
+        }
+    };
+};
+
+template <typename Note = outstream_append_nothing, typename Base = reporter_outstream_formatter_line_common_char<> >
+struct reporter_outstream_formatter_header_char : Base
+{
+    template <typename OutStream>
+    inline static void append_to_stream(OutStream& outStream)
+    {
+        Base::prefix::append_to_stream(outStream);
+        outStream << "started ";
+        Note::append_to_stream(outStream);
+        Base::postfix::append_to_stream(outStream);
+    }
+};
+
+template <typename Base = reporter_outstream_formatter_line_common_char<> >
+struct reporter_outstream_formatter_footer_char : Base
+{
+    template <typename OutStream>
+    inline static void append_to_stream(OutStream& outStream, unsigned int passed, unsigned int failed, unsigned int skipped)
+    {
+        Base::prefix::append_to_stream(outStream);
+        outStream << "complete: " << passed << " passed, " << failed << " failed, " << skipped << " skipped";
+        Base::postfix::append_to_stream(outStream);
+    }
+};
+
+template <
+    typename Header = reporter_outstream_formatter_header_char<>, 
+    typename Footer = reporter_outstream_formatter_footer_char<>,
+    typename LineEnding = outstream_append_slashn>
+struct reporter_outstream_formatter_char
+{
+    typedef Header header;
+    typedef Footer footer;
+    typedef LineEnding line_ending;
+};
+
+template <typename OutStream, typename Formatter = reporter_outstream_formatter_char<> >
+struct reporter_outstream_policies
+{
+    typedef OutStream outstream;
+    typedef typename Formatter::header header;
+    typedef typename Formatter::footer footer;
+    typedef typename Formatter::line_ending line_ending;
+};
+
+// Policy base design is used here in order to make it easy to use different type of streams (i.e.
+// stdout, a file, a pipe, etc.), switch from ASCII to a different character set (i.e. UTF-8,
+// UTF-16, etc.), change the line endings used, and modify or remove the header and/or footer.
+template <typename Policies>
+class reporter_outstream
+{
+G42CORE_MC_NOT_COPYABLE(reporter_outstream)
+public:
+    typedef typename Policies::outstream outstream;
+    typedef typename Policies::header header;
+    typedef typename Policies::footer footer;
+    typedef typename Policies::line_ending line_ending;
+
+    reporter_outstream(outstream& outStream):
+        outstream_(outStream)
+    {
+    }
+
+    void on_tests_starting()
+    {
+        header::append_to_stream(outstream_);
+    }
+
+    template <class T>
+    void message(const T& t)
+    {
+        outstream_ << t;
+        line_ending::append_to_stream(outstream_);
+    }
+
+    template <class T>
+    void message_part(const T& t)
+    {
+        outstream_ << t;
+    }
+
+    void tests_completed(unsigned int passed, unsigned int failed, unsigned int skipped = 0)
+    {
+        footer::append_to_stream(outstream_, passed, failed, skipped);
+    }
+
+private:
+    outstream& outstream_;
+};
+
+} // namespace detail
+
+G42CORE_TEST_END_NAMESPACES
+
+#endif // G42CORE_HG_A533B033358A43CB9FC53C9C607EC0AE
---
+++ library/unit_tests/bootstrap/bootstrap010/basictests_reporter_outstream.cpp
@@ -0,0 +1,81 @@
+/*                   Copyright (C) 2012 Josh Heitzman
+Distributed under the Boost Software License, Version 1.0.
+See accompanying file LICENSE_1_0.txt or online copies at:
+* http://www.boost.org/LICENSE_1_0.txt
+* http://opensource.org/licenses/BSL-1.0
+* http://directory.fsf.org/wiki/License:Boost1.0
+* http://en.wikipedia.org/wiki/Boost_Software_License                */
+
+#include "stdafx.h"
+
+#include "g42core/metacode/classes.hpp"
+#include "g42core/test/namespace.hpp"
+#include "g42core/test/detail/reporter_outstream.hpp"
+
+namespace tut
+{
+    struct basictests_reporter_outstream{};
+    typedef test_group<basictests_reporter_outstream> factory;
+    typedef factory::object object;
+}
+
+namespace
+{
+    tut::factory tf("basictests_reporter_outstream");
+}
+
+namespace tut
+{
+    template<>
+    template<>
+    void object::test<1>()
+    {
+        typedef G42CORE_TEST_NS detail::reporter_outstream<G42CORE_TEST_NS detail::reporter_outstream_policies<std::ostream> > reporter_with_ostream;
+        typedef G42CORE_METACODE_NS ensure_not_copyable<reporter_with_ostream> ensure_not_copyable;
+        {
+        std::string expected_header_content("*** Tests started  ***\n");
+        std::stringstream ss;
+        // Basic test of on_tests_starting
+        reporter_with_ostream reporter(ss);
+        reporter.on_tests_starting();
+        auto contents = ss.str();
+        ensure(contents == expected_header_content);
+        ss.str(std::string());
+        // All three should be included in the content even when 0
+        reporter.tests_completed(0, 0, 0);
+        contents = ss.str();
+        ensure(contents == "*** Tests complete: 0 passed, 0 failed, 0 skipped ***\n");
+        ss.str(std::string());
+        // message should work with literal char string
+        reporter.message("");
+        contents = ss.str();
+        ensure(contents == "\n");
+        ss.str(std::string());
+        // ditto for message_part
+        reporter.message_part("");
+        contents = ss.str();
+        ensure(contents == "");
+        ss.str(std::string());
+        // message should also work with std::string
+        reporter.message(std::string("message 2"));
+        contents = ss.str();
+        ensure(contents == "message 2\n");
+        ss.str(std::string());
+        // ditto for message_part
+        reporter.message_part(std::string("message B"));
+        contents = ss.str();
+        ensure(contents == "message B");
+        ss.str(std::string());
+        // Should be able to invoke on_tests_starting again and get the same content
+        reporter.on_tests_starting();
+        contents = ss.str();
+        ensure(contents == expected_header_content);
+        ss.str(std::string());
+        // Now test that again with numbers other than zero including a multidigit numbers.
+        reporter.tests_completed(1, 12, 1000);
+        contents = ss.str();
+        ensure(contents == "*** Tests complete: 1 passed, 12 failed, 1000 skipped ***\n");
+        ss.str(std::string());
+        }
+    }
+}
